e0ba376042dd877a50c25591eecc1e4a46d4d1a2
AvidanSab <avidansabnani@gmail.com>
2026-03-16 17:02:45 -0400

    Added message to Lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Avidan Sabnani" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"{name}, I hope you are having fun and are learning a lot!")
 
 if __name__ == "__main__":
     main()
